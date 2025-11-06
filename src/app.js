2b257251023aece84a94e2c4678d2e9cdeeafa42
Ariel-Monroy-Jala <ariel.monroy@jalasoft.com>
2025-11-06 17:42:29 -0400

    feat: implementing error handler middleware

--- src/app.js
+++ src/app.js
@@ -2,11 +2,14 @@ import Koa from 'koa';
 import { initConnection } from './db/sequelize.js';
 import { router } from './users/routes.js';
 import { bodyParser } from '@koa/bodyparser';
+import { errorHandler } from './middlewares/error-handler.js';
 
 
 const app = new Koa();
 
 app.use(bodyParser());
+
+app.use(errorHandler);
 
 await initConnection();
 
