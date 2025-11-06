0eb2e3fbdae31cdd9af8a567636bf5e4244a0544
Ariel-Monroy-Jala <ariel.monroy@jalasoft.com>
2025-11-06 17:41:09 -0400

    feat: updates clean response login to works in both get endpoints

--- src/middlewares/clean-response.js
+++ src/middlewares/clean-response.js
@@ -1,8 +1,16 @@
 export const cleanResponse =  async (ctx, next) => {
   await next();
-  const users = ctx.body.data.rows;
-  ctx.body.data.rows = users.map((user) => {
-    return { id: user.id, name: user.name, username: user.username };
-  });
+
+  if (!ctx.body || !ctx.body.data) return
+  if (ctx.body.data.rows) {
+    const users = ctx.body.data.rows;
+    ctx.body.data.rows = users.map((user) => {
+      return { id: user.id, name: user.name, username: user.username };
+    });
+  } else {
+    const user = ctx.body.data;
+    ctx.body.data = { id: user.id, name: user.name, username: user.username };
+  }
+  
 
 }
