0b4cf95c675c5b3b38d1c67976db8a2cb7971ece
Ariel-Monroy-Jala <ariel.monroy@jalasoft.com>
2025-11-06 17:05:48 -0400

    feat: adding user routes to app

--- src/app.js
+++ src/app.js
@@ -1,7 +1,19 @@
 import Koa from 'koa';
+import { initConnection } from './db/sequelize.js';
+import { router } from './users/routes.js';
+import { bodyParser } from '@koa/bodyparser';
+
 
 const app = new Koa();
 
+app.use(bodyParser());
+
+await initConnection();
+
+
+app.use(router.routes()).use(router.allowedMethods());
+
+
 const port = process.env.PORT || 3002
 
-app.listen(port, () => { console.log(`Listening on port ${port}`)});
+app.listen(port, () => { console.log(`Listening on port ${port}`) });
