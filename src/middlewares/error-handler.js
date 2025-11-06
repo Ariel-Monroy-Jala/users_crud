9cd3edcda7f1c7beeaa232bf6d3b7027a5b5c4c0
Ariel-Monroy-Jala <ariel.monroy@jalasoft.com>
2025-11-06 17:40:48 -0400

    feat: adds exception handler

---
+++ src/middlewares/error-handler.js
@@ -0,0 +1,13 @@
+export const errorHandler = async (ctx,next) => {
+  try {
+    await next();
+  } catch (error) {
+    console.log(error);
+    ctx.status = error.statusCode || 500;
+    ctx.body = { 
+      success: false,
+      message: error.message || 'Internal Server Error'
+    };
+    ctx.app.emit('error',error,ctx);
+  }
+} 
