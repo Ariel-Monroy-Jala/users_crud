11ff7b106a15ff41670a15f4629e2ffc398a845a
Ariel-Monroy-Jala <ariel.monroy@jalasoft.com>
2025-11-07 09:49:18 -0400

    feat: update getAllusers response

--- src/middlewares/clean-response.js
+++ src/middlewares/clean-response.js
@@ -2,9 +2,9 @@ export const cleanResponse =  async (ctx, next) => {
   await next();
 
   if (!ctx.body || !ctx.body.data) return
-  if (ctx.body.data.rows) {
-    const users = ctx.body.data.rows;
-    ctx.body.data.rows = users.map((user) => {
+  if (ctx.body.data.elements) {
+    const users = ctx.body.data.elements;
+    ctx.body.data.elements = users.map((user) => {
       return { id: user.id, name: user.name, username: user.username };
     });
   } else {
--- src/users/service.js
+++ src/users/service.js
@@ -5,7 +5,7 @@ export const userService = {
 
   createUser: async (user) => {
     if (!user.name || !user.username || !user.password) {
-      throw new Error('Invalid request');
+      throw new ApiException(400, 'Invalid request');
     }
 
     await userRepository.createUser(user);
@@ -14,33 +14,33 @@ export const userService = {
   updateUser: async (id, user) => {
     if (!id) throw new ApiException(400, 'Invalid request');
     const dbUser = userRepository.getUser(id);
-    if (!dbUser) throw new ApiException(404,'Not found');
+    if (!dbUser) throw new ApiException(404, 'Not found');
     await userRepository.updateUser(id, user);
   },
 
   getUser: async (id) => {
     if (!id) throw new ApiException(400, 'Invalid request');
     const user = await userRepository.getUser(id);
-    if (!user) throw new ApiException(404,'Not found');
+    if (!user) throw new ApiException(404, 'Not found');
     return user;
   },
 
   getUsers: async (page, size, filter) => {
     if (!page) page = 1;
     if (!size) size = 10;
-    if (!filter ) {
+    if (!filter) {
       filter = '%%';
     } else {
       filter = `%${filter}%`;
     }
-    
-    return userRepository.getUsers(parseInt(page), parseInt(size), filter);
+    const users = await userRepository.getUsers(parseInt(page), parseInt(size), filter);
+    return { elements: users.rows, totalElements: users.count, page, size, totalPages: Math.ceil(users.count / size) };
   }
   ,
   deleteUser: async (id) => {
     if (!id) throw new ApiException(400, 'Invalid request');
     const dbUser = userRepository.getUser(id);
-    if (!dbUser) throw new ApiException(404,'Not found');
+    if (!dbUser) throw new ApiException(404, 'Not found');
     await userRepository.deleteUser(id);
   }
 }
