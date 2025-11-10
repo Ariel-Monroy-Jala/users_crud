fe3d0eea63b6ec193ec7d67af75cf5ffe1a0793c
Ariel-Monroy-Jala <ariel.monroy@jalasoft.com>
2025-11-10 14:24:11 -0400

    test: adding tests for user respository and service

---
+++ test/users/repository.spec.js
@@ -0,0 +1,66 @@
+/* eslint-disable no-unused-expressions */
+import { describe, it, beforeEach, afterEach } from 'mocha';
+import sinon from 'sinon';
+import { expect } from 'chai';
+import { UserModel } from '../../src/db/models/user.js';
+import { userRepository } from '../../src/users/repository.js';
+
+const fakeUser = {
+  id: '1234-5678',
+  name: 'Jhon Doe',
+  username: 'jdoe',
+  password: 'superPassword'
+};
+
+const fakeId = '1234-5678';
+
+describe('User repository', () => {
+  let createStub;
+  let updateStub;
+  let findOneStub;
+  let findAllStub;
+  let destroyStub;
+
+  beforeEach(() => {
+    createStub = sinon.stub(UserModel, 'create');
+    updateStub = sinon.stub(UserModel, 'update');
+    findOneStub = sinon.stub(UserModel, 'findOne');
+    findAllStub = sinon.stub(UserModel, 'findAndCountAll');
+    destroyStub = sinon.stub(UserModel, 'destroy');
+  });
+
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  it('Should create a user', async () => {
+    createStub.resolves(fakeUser);
+    await userRepository.createUser(fakeUser);
+    expect(createStub.calledOnceWithExactly(fakeUser)).to.be.true;
+  });
+
+  it('Should update a user', async () => {
+    updateStub.resolves(fakeUser);
+    await userRepository.updateUser(fakeId, fakeUser);
+    expect(updateStub.calledOnceWithExactly(fakeUser, { where: { id: fakeId } })).to.be.true;
+  });
+
+  it('Should get user by Id', async () => {
+    findOneStub.resolves(fakeUser);
+    const result = await userRepository.getUser(fakeId);
+    expect(result.id).to.be.equal(fakeId);
+  });
+
+  it('Should get list of users ', async () => {
+    findAllStub.resolves({ rows: [fakeUser], count: 1 });
+    const result = await userRepository.getUsers(1, 10, '');
+    expect(Array.isArray(result.rows)).to.be.true;
+    expect(result.count).to.be.equal(1);
+  });
+
+  it('Should delete one user', async () => {
+    destroyStub.resolves(1);
+    await userRepository.deleteUser(fakeId);
+    expect(destroyStub.calledOnceWithExactly({ where: { id: fakeId } })).to.be.true;
+  });
+});
---
+++ test/users/service.spec.js
@@ -0,0 +1,133 @@
+import { describe, beforeEach, afterEach, it } from 'mocha';
+import Sinon from 'sinon';
+import { userRepository } from '../../src/users/repository.js';
+import { userService } from '../../src/users/service.js';
+import * as chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+import { ApiException } from '../../src/exceptions/ApiException.js';
+
+chai.use(chaiAsPromised);
+const expect = chai.expect;
+
+const fakeUser = {
+  id: '1234-5678',
+  name: 'Jhon Doe',
+  username: 'jdoe',
+  password: 'superPassword'
+};
+
+const fakeId = '1234-5678';
+
+describe('User service', () => {
+  let createStub;
+  let updateStub;
+  let getStub;
+  let getUsersStub;
+  let deleteStub;
+
+  beforeEach(() => {
+    createStub = Sinon.stub(userRepository, 'createUser');
+    updateStub = Sinon.stub(userRepository, 'updateUser');
+    getStub = Sinon.stub(userRepository, 'getUser');
+    getUsersStub = Sinon.stub(userRepository, 'getUsers');
+    deleteStub = Sinon.stub(userRepository, 'deleteUser');
+  });
+
+  afterEach(() => {
+    Sinon.restore();
+  });
+
+  describe('Creating a user', () => {
+    it('Should create user', async () => {
+      createStub.resolves(fakeUser);
+      await userService.createUser(fakeUser);
+      expect(createStub.calledOnceWithExactly(fakeUser));
+    });
+
+    it('Should throw badrequest Error', async () => {
+      createStub.resolves(fakeUser);
+      await expect(userService.createUser({ ...fakeUser, name: null })).to.be.rejectedWith(ApiException, 'Invalid request');
+    });
+  });
+
+  describe('Updating a user', () => {
+    it('Should update user', async () => {
+      updateStub.resolves(fakeUser);
+      getStub.resolves(fakeUser);
+      await userService.updateUser(fakeId, fakeUser);
+      expect(updateStub.calledOnceWithExactly(fakeId, fakeUser));
+    });
+
+    it('Should throw invalid request error', async () => {
+      updateStub.resolves(fakeUser);
+      await expect(userService.updateUser(null, fakeUser)).to.be.rejectedWith(ApiException, 'Invalid request');
+    });
+
+    it('Should throw Not found error', async () => {
+      updateStub.resolves(null);
+      getStub.resolves(null);
+      await expect(userService.updateUser(fakeId, fakeUser)).to.be.rejectedWith(ApiException, 'Not found');
+    });
+  });
+
+  describe('Getting a user', () => {
+    it('Should get user', async () => {
+      getStub.resolves(fakeUser);
+      const result = await userService.getUser(fakeId);
+      expect(result).to.be.equals(fakeUser);
+    });
+
+    it('Should throw Invalid request error', async () => {
+      getStub.resolves(null);
+      await expect(userService.getUser(null)).to.be.rejectedWith(ApiException, 'Invalid request');
+    });
+
+    it('Should throw Not found error', async () => {
+      getStub.resolves(null);
+      await expect(userService.getUser(fakeId)).to.be.rejectedWith(ApiException, 'Not found');
+    });
+  });
+
+  describe('Getting users', () => {
+    it('Should get list user with one user', async () => {
+      getUsersStub.resolves({ rows: [fakeUser], count: 1 });
+      const result = await userService.getUsers(1, 1, '');
+      expect(result.elements.length).to.be.equal(1);
+      expect(result.totalElements).to.be.equal(1);
+    });
+
+    it('Should get list users with multiple pages', async () => {
+      getUsersStub.resolves({ rows: [fakeUser, fakeUser, fakeUser], count: 10 });
+      const result = await userService.getUsers(1, 3, '');
+      expect(result.elements.length).to.be.equal(3);
+      expect(result.totalElements).to.be.equal(10);
+      expect(result.totalPages).to.be.equal(4);
+    });
+
+    it('Should get list users with no data', async () => {
+      getUsersStub.resolves({ rows: [], count: 0 });
+      const result = await userService.getUsers(1, 10, '');
+      expect(result.elements.length).to.be.equal(0);
+      expect(result.totalElements).to.be.equal(0);
+      expect(result.totalPages).to.be.equal(0);
+    });
+  });
+
+  describe('Deleting User', () => {
+    it('Should delete a user', async () => {
+      deleteStub.resolves(1);
+      getStub.resolves(fakeUser);
+      await userService.deleteUser(fakeId);
+      expect(deleteStub.calledOnceWithExactly(fakeId));
+    });
+
+    it('Should throw Invalid request error', async () => {
+      await expect(userService.deleteUser(null)).to.be.rejectedWith(ApiException, 'Invalid request');
+    });
+
+    it('Should throw Not found error', async () => {
+      getStub.resolves(null);
+      await expect(userService.deleteUser(fakeId)).to.be.rejectedWith(ApiException, 'Not found');
+    });
+  });
+});
