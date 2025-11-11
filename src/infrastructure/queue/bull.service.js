b76932f9291dd996becc95713e900e9045a9699f
Ariel-Monroy-Jala <ariel.monroy@jalasoft.com>
2025-11-11 16:49:46 -0400

    feat: creates bull service

---
+++ src/infrastructure/queue/bull.service.js
@@ -0,0 +1,20 @@
+import { usersQueue } from './bull.js';
+
+export const bullService = {
+  /**
+   * Create a new job in usersQueue
+   * @param {job} job Job data
+   */
+  createJob: async (job) => {
+    console.log(`[Bull service]: creating job ${job.action} in ${job.type}`);
+    usersQueue.add(job);
+    console.log('[Bull service]: job created');
+  }
+};
+
+/**
+ * @typedef job
+ *  @property {object} data data required in the job.
+ *  @property {string} action Action to procces.
+ *  @property {string} type data type
+ */
