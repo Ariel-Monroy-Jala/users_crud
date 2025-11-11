b3a0aed6e465171ca4ef72f2de12eb25aed3ea42
Ariel-Monroy-Jala <ariel.monroy@jalasoft.com>
2025-11-11 16:51:17 -0400

    feat: adding worker

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import Koa from 'koa';
-import { initConnection } from './db/sequelize.js';
+import { initConnection } from './infrastructure/db/sequelize.js';
 import { router } from './users/routes.js';
 import { bodyParser } from '@koa/bodyparser';
 import { errorHandler } from './middlewares/error-handler.js';
+import './infrastructure/worker/worker.js';
+import { testConnection } from './infrastructure/queue/bull.js';
 
 const app = new Koa();
 
@@ -11,6 +13,8 @@ app.use(bodyParser());
 app.use(errorHandler);
 
 await initConnection();
+
+await testConnection();
 
 app.use(router.routes()).use(router.allowedMethods());
 
