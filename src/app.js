3d550239df5b2b2a8e96da18040f520d2ddd7b5b
Ariel-Monroy-Jala <ariel.monroy@jalasoft.com>
2025-11-12 10:05:11 -0400

    Revert "feat: adding worker"
    This reverts commit b3a0aed6e465171ca4ef72f2de12eb25aed3ea42.

--- src/app.js
+++ src/app.js
@@ -1,10 +1,8 @@
 import Koa from 'koa';
-import { initConnection } from './infrastructure/db/sequelize.js';
+import { initConnection } from './db/sequelize.js';
 import { router } from './users/routes.js';
 import { bodyParser } from '@koa/bodyparser';
 import { errorHandler } from './middlewares/error-handler.js';
-import './infrastructure/worker/worker.js';
-import { testConnection } from './infrastructure/queue/bull.js';
 
 const app = new Koa();
 
@@ -13,8 +11,6 @@ app.use(bodyParser());
 app.use(errorHandler);
 
 await initConnection();
-
-await testConnection();
 
 app.use(router.routes()).use(router.allowedMethods());
 
