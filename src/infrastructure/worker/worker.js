3d253f31341313224f2d39ebd340b747de59f093
Ariel-Monroy-Jala <ariel.monroy@jalasoft.com>
2025-11-12 09:54:35 -0400

    feat: updating worker to work with concurrent jobs

--- src/infrastructure/worker/worker.js
+++ src/infrastructure/worker/worker.js
@@ -5,18 +5,23 @@ const actions = {
   'create:user': userService.bulkCreate
 };
 
-usersQueue.process(async (job) => {
+usersQueue.process(2, async (job) => {
   const { action, type, data } = job.data;
   console.log(`[Bull worker]: Starting process job ${job.id} task ${action} in type ${type}`);
+  await sleep(2000);
   const actionFunc = actions[`${action}:${type}`];
   await actionFunc(data);
   return { success: true, count: data.length };
 });
 
 usersQueue.on('completed', (job, result) => {
-  console.log(`Job ${job.id} completed ${result}`);
+  console.log(`[Bull worker]: Job ${job.id} completed ${JSON.stringify(result)}`);
 });
 
 usersQueue.on('failed', (job, err) => {
-  console.error(`Job ${job.id} failed`, err);
+  console.error(`[Bull worker]: Job ${job.id} failed`, err);
 });
+
+const sleep = (ms) => {
+  return new Promise(resolve => setTimeout(resolve, ms));
+};
