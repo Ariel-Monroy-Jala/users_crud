7d259e98264cf575bc67697cf8682cfbdf74441d
Ariel-Monroy-Jala <ariel.monroy@jalasoft.com>
2025-11-13 14:38:59 -0400

    feat: updates queue name

--- src/infrastructure/queue/bull.js
+++ src/infrastructure/queue/bull.js
@@ -1,9 +1,9 @@
 import Queue from 'bull';
 
-export const usersQueue = new Queue('users_queue', { redis: { port: process.env.REDIS_PORT, host: process.env.REDIS_HOST, password: process.env.REDIS_PASSWORD } });
+export const createBulkUsersQueue = new Queue('create_bulk_users_queue', { redis: { port: process.env.REDIS_PORT, host: process.env.REDIS_HOST, password: process.env.REDIS_PASSWORD } });
 
 export const testConnection = async () => {
-  usersQueue.client.ping().then((res) => {
+  createBulkUsersQueue.client.ping().then((res) => {
     console.log('[Bull queue]: Redis connected', res);
   }).catch((err) => {
     console.error('[Bull queue]: Error connecting with Redis', err);
