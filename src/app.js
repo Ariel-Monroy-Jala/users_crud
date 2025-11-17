936daeec923b80b1f13733a1559fc356a70c00e5
Ariel-Monroy-Jala <ariel.monroy@jalasoft.com>
2025-11-17 12:51:57 -0400

    feat: import container in app

--- src/app.js
+++ src/app.js
@@ -3,8 +3,8 @@ import { initConnection } from './infrastructure/db/sequelize.js';
 import { router } from './users/routes.js';
 import { bodyParser } from '@koa/bodyparser';
 import { errorHandler } from './middlewares/error-handler.js';
+import './container.js';
 import './infrastructure/worker/worker.js';
-import { testConnection } from './infrastructure/queue/bull.js';
 
 const app = new Koa();
 
@@ -13,8 +13,6 @@ app.use(bodyParser());
 app.use(errorHandler);
 
 await initConnection();
-
-await testConnection();
 
 app.use(router.routes()).use(router.allowedMethods());
 
